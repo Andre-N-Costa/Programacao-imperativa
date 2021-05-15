0bb4995a24919d702e9dd1b927c7c30ebacddb60
André Costa <74073671+TrappyDie@users.noreply.github.com>
2021-05-15 22:02:22 +0100

    Update 50QC.c

--- 50QC.c
+++ 50QC.c
@@ -80,6 +80,19 @@ int qDig (unsigned int n){
     return i;
 }
 
+//7
+
+char *concat (char s1[], char s2[]){
+    int i,j;
+    for(i = 0; s1[i]; i++);
+    for (j = 0; s2[j]; j++){
+        s1[i] = s2[j];
+        i++;
+    }
+    s1[i] = '\0';
+    return s1;
+}
+
 //8
 
 char *mystrcpy(char s1[], const char s2[]) {
@@ -107,4 +120,51 @@ int mystrcmp (char s1[], char s2[]){
     }
     
     return c;
+}
+
+//10
+
+char *mystrstr (char s1[], char s2[]){
+    int i,pos = 0, j = 0,indice;
+    for (i = 0; s1[i]; i++){
+        if (s1[i] != s2[0]);
+        else {
+            while ((s2[j]) || (s1[pos] == s2[j])) {
+                j++;pos++;
+            }
+            indice = i;
+            if (j == strlen(s2)) s1[i] = '\0';
+            else {j = 0; pos = 0; indice = 0;}
+        }
+    }
+    char *rtrn;
+    if (indice != 0) rtrn = s2;
+    else rtrn = "NULL";
+return rtrn;
+}
+
+//11
+
+void mystrrev (char s[]){
+    char tmp[50];
+    int j = 0;
+    for(int i = (strlen(s) - 1); i >= 0; i--) {
+        tmp[j] = s[i];
+        j++;
+    }
+    tmp[j] = '\0';
+    strcpy(s,tmp);
+}
+
+//12
+
+void strnoV (char s[]){
+    int j = 0;
+    char tmp[50];
+    for(int i = 0; s[i]; i++){
+        if ((s[i] == 'a') || (s[i] == 'e') || (s[i] == 'i') || (s[i] == 'o') || (s[i] == 'u') || (s[i] == 'A') || (s[i] == 'E') || (s[i] == 'I') || (s[i] == 'O') || (s[i] == 'U'));
+        else {tmp[j] = s[i];j++;}
+    }
+    tmp[j] = '\0';
+    strcpy(s,tmp);
 }
